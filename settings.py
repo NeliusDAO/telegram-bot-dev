4c0f73fa4b710b02f578d1bf0c0be87ba74cd96b
Timi-G <rotimiolasehinde.a@gmail.com>
2025-11-10 17:10:07 +0100

    Added file to contain code global and server-end definitions

---
+++ settings.py
@@ -0,0 +1,28 @@
+import os
+import psycopg2
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# --- Config ---
+DATABASE_URL = os.getenv("DATABASE_URL")
+DEV_IDS = [int(x) for x in os.getenv("DEV_IDS", "").split(",") if x]
+REDIS_URL = os.getenv("REDIS_URL", "redis://localhost:6379/0")
+
+# Telegram Bot Info
+TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
+COMMUNITY_LINK = "https://t.me/+P9j1f85xo1ExZTk0"
+
+# Web Hook
+WEBHOOK_URL = f"https://{os.getenv('RENDER_EXTERNAL_HOSTNAME')}/{TELEGRAM_BOT_TOKEN}"
+PORT = int(os.getenv("PORT", 8080))
+
+# --- SQL helper ---
+def get_db_connection():
+    if DATABASE_URL.startswith("postgres"):
+        conn = psycopg2.connect(DATABASE_URL)
+    elif DATABASE_URL.startswith("sqlite"):
+        DB_PATH = DATABASE_URL.replace("sqlite:///", "")
+        import sqlite3
+        conn = sqlite3.connect(DB_PATH)
+    return conn
